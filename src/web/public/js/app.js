fd0b4c221ed1e8b84bc08b27869c96b2258d2204
hyunkim <68850889+aittaa@users.noreply.github.com>
2022-06-01 09:53:44 +0900

    Chore 프론트 코드 순서, 주석 수정

--- src/web/public/js/app.js
+++ src/web/public/js/app.js
@@ -1,54 +1,13 @@
-let socket;
-
-function socketInit(token) {
-  socket = io({
-    query: { token },
-  });
-
-  // on
-  socket.on("connect", () => {
-    console.log("socket connected"); // true
-  });
-  //서버 인증 예외 발생 시 connect_error 이벤트 발생
-  socket.on("connect_error", (data) => {
-    console.log(data.message);
-  });
-
-  socket.on("message", addMessage);
-}
-
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
 const room = document.getElementById("room");
 room.hidden = true;
 
 let roomId;
+let socket;
 
-//메시지 수신 받은 경우
-function addMessage({ accountId, message }) {
-  displayMessage(`${accountId} : ${message}`);
-}
-
-//단순히 화면에 메시지 추가
-function displayMessage(message) {
-  const ul = room.querySelector("ul");
-  const li = document.createElement("li");
-  li.innerText = message;
-  ul.appendChild(li);
-}
 //emit
 form.addEventListener("submit", handleRoomSubmit);
-
-// 방 입장 성공 시 화면 전환
-function showRoom(roomId) {
-  welcome.hidden = true;
-  room.hidden = false;
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomId}`;
-
-  const msgForm = room.querySelector("#msg");
-  msgForm.addEventListener("submit", handleMessageSubmit);
-}
 
 //방 입장 버튼 눌렸을 때 핸들러
 function handleRoomSubmit(event) {
@@ -76,4 +35,46 @@ function handleMessageSubmit(event) {
   socket.emit("message", { message }, () => {
     displayMessage(`You : ${message}`); // 성공 시 내 메시지 바로 display
   });
+}
+
+// 방 입장 시의 socket 초기화
+function socketInit(token) {
+  socket = io({
+    query: { token },
+  });
+
+  // on
+  socket.on("connect", () => {
+    console.log("socket connected"); // true
+  });
+  //서버 인증 예외 발생 시 connect_error 이벤트 발생
+  socket.on("connect_error", (data) => {
+    console.log(data.message);
+  });
+
+  socket.on("message", addMessage);
+}
+
+//메시지 수신 받은 경우
+function addMessage({ accountId, message }) {
+  displayMessage(`${accountId} : ${message}`);
+}
+
+//단순히 화면에 메시지 추가
+function displayMessage(message) {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message;
+  ul.appendChild(li);
+}
+
+// 방 입장 성공 시 화면 전환
+function showRoom(roomId) {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomId}`;
+
+  const msgForm = room.querySelector("#msg");
+  msgForm.addEventListener("submit", handleMessageSubmit);
 }
